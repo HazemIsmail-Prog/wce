f2c9464aa966849c8f0281d4ad02f7b428ff4715
HazemIsmail-Prog <hazem.ismail@hotmail.com>
2022-11-20 17:15:48 +0300

    Edit web.php

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::post('/save_estimation/{game}',[EstimationController::class,'store'])->name('save_estimation');
-// Route::get('/', function () {
-//     return view('home');
-// })->middleware(['auth', 'verified'])->name('home');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::post('/save_estimation/{game}', [EstimationController::class, 'store'])->name('save_estimation');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
