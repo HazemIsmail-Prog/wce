fc16bbdc20de1e1dfc9898d143ed28f17aac8b6f
HazemIsmail-Prog <hazem.ismail@hotmail.com>
2022-11-21 08:47:56 +0300

    add week day to date

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,8 @@
                             <div class="block rounded-lg shadow-lg bg-white max-w-sm w-full overflow-hidden ">
                                 <div
                                     class=" flex justify-between items-center bg-teal-600 p-4 text-white leading-tight font-medium">
-                                    <h5>{{ $game->date_time->format('d-m-Y') }}</h5>
-                                    <h5 class="text-sm">{{ $game->date_time->format('h:i a') }}</h5>
+                                    <h5 class=" text-sm">{{ $game->date_time->format('D,  d-m-Y') }}</h5>
+                                    <h5 class="text-xs">{{ $game->date_time->format('h:i a') }}</h5>
                                 </div>
                                 <div class=" p-4 bg-white">
                                     @if ($game->date_time > now()->addMinutes(15))
@@ -74,7 +74,7 @@
                                                         {{ @$row->user->name }}
                                                         @if ($row->score > 0)
                                                             <span
-                                                                class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-500 text-white rounded-full">{{ $row->score }}</span>
+                                                                class="inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-500 text-white rounded-full" style="font-size: 10px">{{ $row->score }}</span>
                                                         @endif
                                                     </td>
                                                     <td class=" flex justify-between py-2 items-center">
